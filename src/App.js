d99e16df78670c1181fe76aab2b8c6a31e1a45d9
Miaherbillon <herbillon.mia@gmail.com>
2023-10-30 11:01:32 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+
+export default function App() {
+  return (
+    <section>
+      <h1>Exemple Trello 
+      </h1>
+      <section className="colone">
+        <div>
+          <h2>
+            Liste des tableaux
+          </h2> 
+          <section>
+            <button
+        onClick={() => {
+         
+        }}
+      >
+       Create</button>
+          </section>
+          
+        
+      
+        </div>
+      <div> 
+      <h2>
+      Cartes
+     </h2>
+  
+
+      </div>
+      </section>
+    </section>
+  );
+}
+
