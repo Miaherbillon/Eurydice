71f64f812d930ac4f40f1182a62077f16d0847ae
Miaherbillon <herbillon.mia@gmail.com>
2023-10-30 11:10:53 +0100

    Mise en page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import './App.css';
-
+import Liste from "./components/liste.js"
+import Cartes from "./components/cartes.js"
 
 export default function App() {
   return (
     <section>
-      <h1>Exemple Trello 
-      </h1>
-      <section className="colone">
-        <div>
-          <h2>
-            Liste des tableaux
-          </h2> 
-          <section>
-            <button
-        onClick={() => {
-         
-        }}
-      >
-       Create</button>
-          </section>
-          
-        
-      
-        </div>
-      <div> 
-      <h2>
-      Cartes
-     </h2>
-  
-
+      <h1>Titre</h1>
+      <div>
+         <Liste />
+         <Cartes/>
       </div>
-      </section>
+     
     </section>
   );
 }
-
